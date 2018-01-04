5e24af2351f01488d83a047dd84eff4b51f217d0
ns <2294644975@qq.com>
2018-01-04 23:41:29 +0800

    改api

--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -1,5 +1,6 @@
 export function getApi () {
     return {
-        baseURL:'http://localhost:8080/api'
+        // baseURL:'http://localhost:8080/api'
+        baseURL:'http://39.104.64.247:8080/api'
     }
 }
