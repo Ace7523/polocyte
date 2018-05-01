4fd663a2bcc8d45464a82ed9dc8ffcfc53cacb14
nishuai <nishuai@zhuanzhuan.com>
2018-05-01 22:10:33 +0800

    1

--- src/api/apiconfig.js
+++ src/api/apiconfig.js
@@ -1,5 +1,6 @@
 export function getApi () {
     return {
-        baseURL:'http://localhost:8080/api'
+        // baseURL:'http://localhost:8080/api'
+        baseURL:'http://120.79.149.220:8080/api'
     }
 }
